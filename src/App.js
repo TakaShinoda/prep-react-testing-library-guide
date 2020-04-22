a5e1d7516c2da1704ac81610d33c93eb57c9c49d
Takahiro <tkhr_621@docomo.ne.jp>
2020-04-22 22:35:15 +0900

    Fix: アロー関数に変更

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <h1>Testing Updated</h1>
@@ -9,5 +9,3 @@ function App() {
   );
 }
 
-
-export default App;
